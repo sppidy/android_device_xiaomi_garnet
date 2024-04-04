05ebc2f04d4553485b49c4ab45a5ad098f06a1e3
Sugakesshaa <sugakesshaa@pixelos.net>
2024-04-05 01:20:50 +0800

    garnet: Build android.hardware.graphics.common-V4-ndk
    AOSP upgraded the blob to V5 but xiaomi's libgui requires V4 version of the blob
    
    Change-Id: Ib596cde74c414bcb5ac2ea33c394ad7b83b336f0
    Signed-off-by: Coptan99 <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -136,6 +136,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.7.vendor \
+    android.hardware.graphics.common-V4-ndk \
     libexif.vendor \
     liblz4.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
