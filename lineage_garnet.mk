101194cc9bdac7a8ae6e87bdf4716ac0cb4f29f2
Omar Mohamed <omarcoptan9@gmail.com>
2024-04-28 16:34:11 +0000

    garnet: Initialize for Evolution X 9.x
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from garnet device
@@ -16,6 +16,8 @@ $(call inherit-product, device/xiaomi/garnet/device.mk)
 
 # Inherit from the MiuiCamera setup
 $(call inherit-product-if-exists, vendor/xiaomi/garnet-miuicamera/products/miuicamera.mk)
+
+TARGET_BUILD_APERTURE_CAMERA := false
 
 PRODUCT_NAME := lineage_garnet
 PRODUCT_DEVICE := garnet
