49890110a070f61b07b921a5c34ca34d8a668004
Coptan99 <omarcoptan9@gmail.com>
2024-07-17 22:46:40 +0000

    Revert "garnet: Add support for udfps long press sensor"
    This reverts commit 802bbc2b6a1fc6489e2d3db12ce6c10258230dd9.

--- device.mk
+++ device.mk
@@ -430,10 +430,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge \
-    sensors.xiaomi
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
